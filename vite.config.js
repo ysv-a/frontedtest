6a3562aedf15b00470102c38b35d8750cabb9770
Alex <ysov-p@mail.ru>
2025-09-02 16:26:45 +0700

    инцициализирует проект

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  base: '/frontedtest/',
+  build: {
+    emptyOutDir: true,
+    modulePreload: { polyfill: false },
+    rollupOptions: {
+      input: {
+        index: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
+      },
+    },
+  },
+});
